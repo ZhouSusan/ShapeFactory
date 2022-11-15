e22161193134de7e5d41be5ee6e09af1734e94ed
ZhouSusan <szhou089@gmail.com>
2022-11-14 20:41:21 -0800

    added DisplayArea() to show the shape's properties in ShapeFactory.java

--- src/main/java/org/example/ShapeFactory.java
+++ src/main/java/org/example/ShapeFactory.java
@@ -1,6 +1,13 @@
 package org.example;
+import base.ShapeBase;
 
 public class ShapeFactory {
+
+    public void DisplayArea(ShapeBase base) {
+        System.out.format("Display the area for the shape's name: %s , width = %s and height = %s." ,
+                base.getName(), base.getWidth(), base.getHeight());
+        System.out.format("Total area for %s is %s.", base.getName(), base.calculateArea());
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
