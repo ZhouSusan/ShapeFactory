2d94d3d369d72e7ff4821c396787dca775927c45
ZhouSusan <szhou089@gmail.com>
2022-11-14 19:47:43 -0800

    added protected variables with getters/setters in ShapeBase.java

--- src/main/java/base/ShapeBase.java
+++ src/main/java/base/ShapeBase.java
@@ -1,4 +1,35 @@
 package base;
 
-public class ShapeBase {
+public class ShapeBase implements ShapeInterface{
+    protected String name;
+    protected int width, height;
+
+    @Override
+    public int calculateArea() {
+        return 0;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setWidth(int w) {
+        this.width = w;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int h) {
+        this.height = h;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
--- src/main/java/base/ShapeInterface.java
+++ src/main/java/base/ShapeInterface.java
@@ -1,4 +1,5 @@
 package base;
 
 public interface ShapeInterface {
+    int calculateArea();
 }
